d9431bb1956902f41c635ff773e4dc07cd8d2598
webazex <webazex@gmail.com>
2021-06-07 13:45:55 +0300

    temp-fix 4

--- wp-content/themes/Education/back/mail/sender.php
+++ wp-content/themes/Education/back/mail/sender.php
@@ -1,6 +1,6 @@
 <?php
 $headers = "Content-type: text/html; charset=utf-8 \r\n";
-$headers .= "info@pl-education.com.ua\r\n";
+$headers .= "From: info@pl-education.com.ua\r\n";
 $headers .= "Reply-To: info@pl-education.com.ua\r\n";
 require $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
 //$to = get_post_meta(15, 'callback_email')[0];
